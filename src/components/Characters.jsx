e6021dadab8bdecef50e3dacfd1402ba572ef5f5
akibhasan50 <tonuakibhasan@gmail.com>
2022-12-18 16:59:31 +0600

    pagination integrated

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -4,13 +4,16 @@ import { useQuery } from "react-query";
 import Character from "./Character";
 
 export default function Characters() {
-
-  const fetchCharacters = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-
+  const [page, setPage] = useState(1);
+  const fetchCharacters = async ({ queryKey }) => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
     return response.json();
   };
-  const { data, status } = useQuery("characters", fetchCharacters);
+  const { data, status } = useQuery(["characters", page], fetchCharacters, {
+    keepPreviousData: true,
+  });
 
   if (status === "loading") {
     return <div>Loading...</div>;
@@ -20,10 +23,25 @@ export default function Characters() {
   }
 
   return (
-    <div>
+    <div className="characters">
       {data.results.map((character, index) => {
         return <Character key={index} character={character}></Character>;
       })}
+
+      <div>
+        <button
+          disabled={page === 1}
+          onClick={() => setPage((old) => Math.max(old - 1, 1))}
+        >
+          Previous
+        </button>
+        <button
+          disabled={!data.info.next}
+          onClick={() => setPage((old) => old + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
